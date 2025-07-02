415db099893fa195ee24c71687c477c86ea5fa0f
wandajulianto <wandajuianto37@gmail.com>
2025-07-02 14:57:59 +0700

    hapus icon tabler

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,11 +3,6 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-menu" aria-controls="sidebar-menu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <h1 class="navbar-brand navbar-brand-autodark">
-      <a href=".">
-        <img src="{{ asset('assets/img/logo-white.svg') }}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
-      </a>
-    </h1>
     <div class="navbar-nav flex-row d-lg-none">
       <div class="nav-item d-none d-lg-flex me-3">
         <div class="btn-list">
